3d2a830a07c59ae2da9ae9d7bcc0f6f51ae68774
Namish <namish907@gmail.com>
2026-07-16 11:17:05 +0530

    yaml checks 5

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ app.get("/", (_req, res) => {
 app.get("/health", (_req, res) => {
   res.json({ message: "ci-cd pipeline is working fine with health check " });
 });
+
+app.get("/status", (_req, res) => {
+  res.json({ message: "ci-cd pipeline is working fine with status check " });
+});
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
